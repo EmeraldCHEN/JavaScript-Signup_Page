9ed644ad6b91b8440abf218e6263d3e08d62ef7e
EmeraldNZ <ahyuancy@gmail.com>
2018-09-18 10:49:46 +1200

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,13 +1,129 @@
-function signupCheck(){
-  var textvalue = document.getElementByName('phone')[0].value;
-  if (textvalue.length != 11)
-  {
-   document.getElementById('phonemsg').innerHTML = '11 DIGITS PLEASE';
-  } eles {
-      for (var i=0;i<textvalue.length;i++){
-        if (textvalue.substr(i,1)<"0" || textvalue.substr(i,1)>"9"){
-        document.getElementById('phonemsg').innerHTML = 'ALL NUMBERS PLEASE';
-        }
-      }
-  }
+function registCheckXXXXXXXXXXX(){
+    var textValue = document.getElementsByName('phone')[0].value;
+    
+    if(textValue.length != 11){
+        document.getElementById('phonemsg').innerHTML = 'Please fill in 11 digits.'
+    }            
+    else{
+        for(var i = 0; i < textValue.length; i++){
+            if(textValue.substr(i,1) < "0" || textValue.substr(i,1) > "9"){
+                document.getElementById('phonemsg').innerHTML = 'Only digits please.'
+            }
+        }    
+    }
+
+    var textValue1 = document.getElementsByName('password')[0].value;
+    if(textValue1.length < 8){
+        document.getElementById('passwordmsg').innerHTML='No shorter than 8 digits';
+    }
+    var textValue2 = document.getElementsByName('passwordDoubleCheck')[0].value;
+    if(textValue2.length < 8){
+    document.getElementById('passwordDoubleCheckmsg').innerHTML = 'No shorter than 8 digits';
 }
+    else{
+        if(textValue1 != textValue2){
+            document.getElementById('passwordDoubleCheckmsg').innerHTML = 'Not the same password';
+        }
+    }
+    var textValue3 = document.getElementsByName('username')[0].value;
+    if(textValue3.length > 10){
+        document.getElementById('usernamemsg').innerHTML = 'No longer than 10 digits';
+    }
+    var textValue4 = document.getElementsByName('sign')[0].value;
+    if(textValue4.length >10){
+        document.getElementById('signmsg').innerHTML = 'No longer than 10 digits';
+    }
+
+}
+
+function clearPhoneMsg(){
+    document.getElementById('phonemsg').innerHTML = '';
+}
+
+function phoneOnfocus(){
+   
+    document.getElementById('phonemsg').innerHTML = '';
+}
+function phoneOnblur(){
+   var textValue = document.getElementsByName('phone')[0].value;       // ducument ...... SPELLING!!!!!
+                    
+   if(textValue.length != 11){
+        document.getElementById('phonemsg').innerHTML = 'Must be 11 digits';
+   }
+   else{
+        for(var i = 0; i < textValue.length; i++){
+            if(textValue.substr(i,1)<"0" || textValue.substr(i,1) > "9"){
+                document.getElementById('phonemsg').innerHTML = 'ONLY number';
+            }
+        }
+    }    
+}
+function passwordOnfocus(){
+    document.getElementById('passwordmsg').innerHTML = '';
+}
+function passwordOnblur(){
+    var textvalue1 = document.getElementsByName('password')[0].value;
+    if(textvalue1.length < 8){
+        document.getElementById('passwordmsg').innerHTML = 'No shorter than 8 digits';
+    }  
+}
+
+function passwordDoubleCheckOnfocus(){
+    document.getElementById('passwordDoubleCheckmsg').innerHTML = '';
+}
+function passwordDoubleCheckOnblur(){
+    var textValue1 = document.getElementsByName('password')[0].value; 
+    
+    // wITHOUT the above, ERROR: textValue1 is not defined.
+    
+    var textValue2 = document.getElementsByName('passwordDoubleCheck')[0].value;
+    if(textValue2.length < 8){
+        document.getElementById('passwordDoubleCheckmsg').innerHTML = 'No shorter than 8 digits';
+    }
+    else{
+        if(textValue1  !=  textValue2){
+            document.getElementById('passwordDoubleCheckmsg').innerHTML = 'Not the same password';
+        }
+    }
+}
+function usernameOnfocus(){
+    document.getElementById('usernamemsg').innerHTML = "";
+}
+function usernameOnblur(){
+    var textValue3 = document.getElementsByName('username')[0].value;
+    if(textValue3.length > 10){
+        document.getElementById('usernamemsg').innerHTML = "No longer than 10 digits";
+    }
+}
+function signOnfocus(){
+    document.getElementById('signmsg').innerHTML = "";
+}
+function signOnblur(){
+    var textvalue4 = document.getElementsByName('sign')[0].value;
+    if(textvalue4.length > 10){
+        document.getElementById('signmsg').innerHTML = "No longer than 10 digits";
+    }
+}
+
+function registCheck(){
+     phoneOnblur();
+     passwordOnblur();
+     passwordDoubleCheckOnblur();
+     usernameOnblur();
+     signOnblur();
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
